4fbac97433abe5d3f9a0d3226a961da6d653724c
Ally Morson <ally.morson@gmail.com>
2019-09-28 05:45:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,9 @@
 def oxford_comma(array)
- ["kiwi"].join
- end
- 
- def oxford_comma(array)
-   ["kiwi", "apple"].join ("and")
-   
- end
- 
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")    
+end
    
